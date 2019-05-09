fcf4390858997963a85cc565008f3bc166e3e3e8
hanjx16 <2658448319@qq.com>
2019-05-09 23:41:00 +0800

    run in mac

--- xgb.py
+++ xgb.py
@@ -7,7 +7,8 @@ xgb对分类数据使用
 import xgboost as xgb
 from sklearn.metrics import accuracy_score
 from xgboost import XGBClassifier
-
+from matplotlib import pyplot as plt
+from xgboost import plot_tree
 
 path = './data/'
 dtrain = xgb.DMatrix(path + 'agaricus.txt.train')
@@ -35,27 +36,35 @@ def evaluate(data=dtest, model=bst):
 
 print(evaluate())
 
-from sklearn.model_selection import GridSearchCV
-from sklearn.datasets import load_svmlight_file
 
-x_train, y_train = load_svmlight_file(path + 'agaricus.txt.train')
-x_test, y_test = load_svmlight_file(path + 'agaricus.txt.test')
+# can run in mac
 
-# x_train, x_dev, y_train, y_dev = train_test_split(x_train, y_train, test_size=0.2)
+plot_tree(booster=bst)
 
-bst = XGBClassifier(max_depth=2, learning_rate=0.1, n_estimators=num_round, silent=0, objective='binary:logistic')
+plt.show()
 
 
-def evaluate_sklearn(data, model, label):
-	pred = model.predict(data)
-	pred_res = [round(res) for res in pred]
-	acc = accuracy_score(label, pred_res)
-	return acc
-
-param_test = {
-	'n_estimators': range(1, 50, 1)
-}
-
-clf = GridSearchCV(estimator=bst, param_grid=param_test, scoring='accuracy', cv=5)
-clf.fit(x_train, y_train)
-print(evaluate_sklearn(x_test, clf.best_estimator_, y_test))
+# from sklearn.model_selection import GridSearchCV
+# from sklearn.datasets import load_svmlight_file
+#
+# x_train, y_train = load_svmlight_file(path + 'agaricus.txt.train')
+# x_test, y_test = load_svmlight_file(path + 'agaricus.txt.test')
+#
+# # x_train, x_dev, y_train, y_dev = train_test_split(x_train, y_train, test_size=0.2)
+#
+# bst = XGBClassifier(max_depth=2, learning_rate=0.1, n_estimators=num_round, silent=0, objective='binary:logistic')
+#
+#
+# def evaluate_sklearn(data, model, label):
+# 	pred = model.predict(data)
+# 	pred_res = [round(res) for res in pred]
+# 	acc = accuracy_score(label, pred_res)
+# 	return acc
+#
+# param_test = {
+# 	'n_estimators': range(1, 50, 1)
+# }
+#
+# clf = GridSearchCV(estimator=bst, param_grid=param_test, scoring='accuracy', cv=5)
+# clf.fit(x_train, y_train)
+# print(evaluate_sklearn(x_test, clf.best_estimator_, y_test))
